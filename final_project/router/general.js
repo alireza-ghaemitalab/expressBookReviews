c7f746942da5ccf672d56798c7d16e8212a7c9fe
علیرضا قایمی <agt.alireza@gmail.com>
2023-12-05 00:41:26 +0330

    Add axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
  
 const doesExist = (username)=>{
   let userswithsamename = users.filter((user)=>{
@@ -31,6 +32,55 @@ public_users.post("/register", (req,res) => {
   } 
   return res.status(404).json({message: "Unable to register user."});
 });
+
+
+// Get the book list available in the shop
+public_users.get('/', async function (req, res) {
+  try {
+      const response = await axios.get('URL_TO_GET_BOOKS');
+      const books = response.data;
+      res.status(200).json(books);
+  } catch (error) {
+      res.status(500).json({ message: "Error fetching books", error: error.message });
+  }
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn;
+  try {
+      const response = await axios.get(`URL_TO_GET_BOOK_BY_ISBN/${isbn}`); // Replace with the actual URL or API endpoint
+      const book = response.data;
+      res.status(200).json(book);
+  } catch (error) {
+      res.status(500).json({ message: "Error fetching book details", error: error.message });
+  }
+});
+
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+  const author = req.params.author;
+  try {
+      const response = await axios.get(`URL_TO_GET_BOOKS_BY_AUTHOR/${author}`);
+      const booksByAuthor = response.data;
+      res.status(200).json(booksByAuthor);
+  } catch (error) {
+      res.status(500).json({ message: "Error fetching books by author", error: error.message });
+  }
+});
+
+// Get all books based on title
+public_users.get('/title/:title', async function (req, res) {
+  const title = req.params.title;
+  try {
+      const response = await axios.get(`URL_TO_GET_BOOKS_BY_TITLE/${title}`);
+      const booksByTitle = response.data;
+      res.status(200).json(booksByTitle);
+  } catch (error) {
+      res.status(500).json({ message: "Error fetching books by title", error: error.message });
+  }
+});
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
